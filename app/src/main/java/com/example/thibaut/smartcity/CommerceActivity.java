7b5f5d70af12d46becd92a1734321ac4742bc5a0
Theo <theo-clayette@orange.fr>
2018-04-05 13:46:54 +0200

    j'sais plus

--- app/src/main/java/com/example/thibaut/smartcity/CommerceActivity.java
+++ app/src/main/java/com/example/thibaut/smartcity/CommerceActivity.java
@@ -1,8 +1,11 @@
 package com.example.thibaut.smartcity;
 
+import android.app.ActionBar;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;

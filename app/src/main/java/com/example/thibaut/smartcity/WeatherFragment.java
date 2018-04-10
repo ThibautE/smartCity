3f9eb87f2aa320299788b1269ea519aa42574575
Theo <theo-clayette@orange.fr>
2018-04-10 11:07:36 +0200

    Ajout des icones météo Ajout d'une imageView accueillant l'icone météo Tentative d'appeler l'icone dans l'imageView //TODO debug l'appel ci-dessus

--- app/src/main/java/com/example/thibaut/smartcity/WeatherFragment.java
+++ app/src/main/java/com/example/thibaut/smartcity/WeatherFragment.java
@@ -5,9 +5,11 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,6 +26,8 @@ import com.example.thibaut.smartcity.DB.MainDB;
 import com.example.thibaut.smartcity.DB.PersonDB;
 import com.example.thibaut.smartcity.function.WeatherFunction;
 
+import static android.content.ContentValues.TAG;
+
 public class WeatherFragment extends Fragment {
 
     Handler handler;
@@ -33,6 +37,7 @@ public class WeatherFragment extends Fragment {
     TextView weatherTextView;
     TextView humidityTextView;
     TextView temperatureTextView;
+    ImageView weatherImageView;
 
     public WeatherFragment() {
         handler = new Handler();
@@ -45,6 +50,7 @@ public class WeatherFragment extends Fragment {
         weatherTextView = rootView.findViewById(R.id.weather);
         humidityTextView = rootView.findViewById(R.id.humidity);
         temperatureTextView = rootView.findViewById(R.id.temperature);
+        weatherImageView = rootView.findViewById(R.id.weathericon);
         return rootView;
     }
 
@@ -122,27 +128,29 @@ public class WeatherFragment extends Fragment {
             iconM = "Nuit";
         switch (id) {
             case 200:
-                icon = "Orage";
+                icon = "orage";
                 break;
             case 300:
-                icon = "Bruine";
+                icon = "bruine";
                 break;
             case 700:
-                icon = "Brouillard";
+                icon = "brouillard";
                 break;
             case 800:
-                icon = "Beau";
+                icon = "beau";
                 break;
             case 801:
-                icon = "Nuageux";
+                icon = "nuageux";
                 break;
             case 600:
-                icon = "Neige";
+                icon = "neige";
                 break;
             case 500:
-                icon = "Pluie";
+                icon = "pluie";
                 break;
         }
-        weatherTextView.setText(iconM + ", " + icon);
+        weatherTextView.setText(iconM + ", " + icon); //ajoute la météo correspondante dans la textView
+        //TODO NE MARCHE PAS ;_;
+        weatherImageView.setImageResource(getResources().getIdentifier(icon, "drawable", "package.name")); //est censé ajouter l'image correspondante a la météo dans l imageview
     }
 }
